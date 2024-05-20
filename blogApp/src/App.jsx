a61547a11167b4e5dcfb4d53512e3f297a1f05af
Devendra <devendrav1910@gmail.com>
2024-05-20 19:13:24 +0530

    adding redux and introduce some components

--- blogApp/src/App.jsx
+++ blogApp/src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState , useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-  console.log(import.meta.env.VITE_APPWRITEE_URL);
-  return (
-    <>
-      <h1>React with appwrite which is similer to google firebase</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+
+  const dispatch = useDispatch()
+
+  useEffect(()=> {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if(userData){
+          dispatch(login({userData}))
+        } else {
+          dispatch(logout)
+        }
+      })
+      .catch(err => console.error(err))
+      .finally(()=>{
+        setLoading(false)
+      })
+    // finally se code run hota hi hai chahe vo error hi return kare
+  })
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400' >
+      <div className='w-full block'>
+        <Header />
+          <main>
+            This is main Body
+            {/* <outlet /> */}
+          </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
