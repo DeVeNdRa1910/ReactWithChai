c06cd06b9174e72ab33333778dbba41073063711
Devendra <devendrav1910@gmail.com>
2024-07-29 17:53:01 +0530

    updating api key of tinyMCe

--- megaBlog/src/components/RTE.jsx
+++ megaBlog/src/components/RTE.jsx
@@ -12,7 +12,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-            apiKey="po0ppkills0klfhexe5qn7a9oya21spf7h4ocxrk0pwu3el4"
+            apiKey="ka5l0l6fpb3g0i2pb9ui72ytiow8dt9sa5f9jii2wsgdzqlc"
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
